c5bbc0c4a8c7c059975f7094197ce67d73ace7f6
Alvaro De la hoz <alvarodlz1999@gmail.com>
2023-12-22 10:22:25 -0500

    I added tailwindcss to the project

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <div className="">
+            <nav>
+                <ul>
+                    <li><link className="" to=""></link></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default Header;
---
+++ src/screens/landingPage.jsx
@@ -0,0 +1,9 @@
+function LandingPage(){
+    return(
+        <div className="landingPage">
+            <h1 className="text-5xl font-bold0 bg-black text-white">Hola</h1>
+        </div>
+    )
+}
+
+export default LandingPage;
